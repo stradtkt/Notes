20fdd535d9d587307b1e098d2105b549bcead552
Kevin <stradtkt22@gmail.com>
2019-08-27 07:30:51 -0400

    Add note menu

--- app/src/main/java/com/stradtman/notes/MainActivity.java
+++ app/src/main/java/com/stradtman/notes/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -16,6 +19,25 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     static ArrayList<String> notes = new ArrayList<>();
     static ArrayAdapter<String> arrayAdapter;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if(item.getItemId() == R.id.add_note) {
+            Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return false;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/stradtman/notes/NoteEditorActivity.java
+++ app/src/main/java/com/stradtman/notes/NoteEditorActivity.java
@@ -9,7 +9,7 @@ import android.text.TextWatcher;
 import android.widget.EditText;
 
 public class NoteEditorActivity extends AppCompatActivity {
-
+    int noteId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,9 +17,13 @@ public class NoteEditorActivity extends AppCompatActivity {
 
         EditText addNote = (EditText) findViewById(R.id.addNote);
         Intent intent = getIntent();
-        final int noteId = intent.getIntExtra("noteId", -1);
+        noteId = intent.getIntExtra("noteId", -1);
         if(noteId != -1) {
             addNote.setText(MainActivity.notes.get(noteId));
+        } else {
+            MainActivity.notes.add("");
+            noteId = MainActivity.notes.size() - 1;
+            MainActivity.arrayAdapter.notifyDataSetChanged();
         }
 
         addNote.addTextChangedListener(new TextWatcher() {
