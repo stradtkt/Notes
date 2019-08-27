bb6037a88c8de0ad9c7f7c525f87595989600569
Kevin <stradtkt22@gmail.com>
2019-08-27 06:37:49 -0400

    Got an example note

--- app/src/main/java/com/stradtman/notes/MainActivity.java
+++ app/src/main/java/com/stradtman/notes/MainActivity.java
@@ -3,12 +3,23 @@ package com.stradtman.notes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
+    ArrayList<String> notes = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        notes.add("Example note");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, notes);
+        listView.setAdapter(arrayAdapter);
     }
 }
